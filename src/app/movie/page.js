37cac158005b7f381985de7b904d41927758045c
arvindk07 <info.arvind07@gmail.com>
2023-06-20 15:45:56 +0530

    fetch api data and set movie and single page

--- src/app/movie/page.js
+++ src/app/movie/page.js
@@ -1,5 +1,35 @@
-const page = () => {
-  return <div>Movie</div>;
+import Link from "next/link";
+import MovieCard from "../components/MovieCard";
+import styles from "@/app/styles/common.module.css";
+
+const Movie = async () => {
+  const url = process.env.RAPID_KEY;
+
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "9329c0d98emshd8e86d3431433cdp1a5c65jsn71ace810a4e8",
+      "X-RapidAPI-Host": "netflix54.p.rapidapi.com",
+    },
+  };
+
+  const res = await fetch(url, options);
+  const data = await res.json();
+  const main_data = data.titles;
+  console.log(main_data.jawSummary);
+
+  return (
+    <div className={styles.movieSection}>
+      <div className={styles.container}>
+        <h1>Movie and Series</h1>
+        <div className={styles.card_section}>
+          {main_data.map((curElem) => {
+            return <MovieCard key={curElem.id} {...curElem} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default Movie;
